3dc6a52df00c0bc562fb6088ac0f78552bdc3424
mrtn1slmn2 <ag447763.martin@gmail.com>
2020-10-03 18:18:57 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Stopwatch0003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbHours = new System.Windows.Forms.Label();
+            this.lbMinutes = new System.Windows.Forms.Label();
+            this.lbSeconds = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lstBxElapsed = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbElapsed = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbHours
+            // 
+            this.lbHours.AutoSize = true;
+            this.lbHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHours.Location = new System.Drawing.Point(41, 86);
+            this.lbHours.Name = "lbHours";
+            this.lbHours.Size = new System.Drawing.Size(53, 37);
+            this.lbHours.TabIndex = 0;
+            this.lbHours.Text = "Hr";
+            // 
+            // lbMinutes
+            // 
+            this.lbMinutes.AutoSize = true;
+            this.lbMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMinutes.Location = new System.Drawing.Point(182, 86);
+            this.lbMinutes.Name = "lbMinutes";
+            this.lbMinutes.Size = new System.Drawing.Size(71, 37);
+            this.lbMinutes.TabIndex = 1;
+            this.lbMinutes.Text = "Min";
+            // 
+            // lbSeconds
+            // 
+            this.lbSeconds.AutoSize = true;
+            this.lbSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSeconds.Location = new System.Drawing.Point(326, 86);
+            this.lbSeconds.Name = "lbSeconds";
+            this.lbSeconds.Size = new System.Drawing.Size(74, 37);
+            this.lbSeconds.TabIndex = 2;
+            this.lbSeconds.Text = "Sec";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(35, 168);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(146, 151);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Timer";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(235, 168);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(165, 151);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop / Reset ";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lstBxElapsed
+            // 
+            this.lstBxElapsed.FormattingEnabled = true;
+            this.lstBxElapsed.ItemHeight = 25;
+            this.lstBxElapsed.Location = new System.Drawing.Point(436, 157);
+            this.lstBxElapsed.Name = "lstBxElapsed";
+            this.lstBxElapsed.Size = new System.Drawing.Size(255, 279);
+            this.lstBxElapsed.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbElapsed
+            // 
+            this.lbElapsed.AutoSize = true;
+            this.lbElapsed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbElapsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbElapsed.Location = new System.Drawing.Point(614, 71);
+            this.lbElapsed.Name = "lbElapsed";
+            this.lbElapsed.Size = new System.Drawing.Size(2, 57);
+            this.lbElapsed.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(429, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 42);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Last Elapsed Time (s):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 37);
+            this.label3.TabIndex = 9;
+            this.label3.Text = ":";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 457);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbElapsed);
+            this.Controls.Add(this.lstBxElapsed);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbSeconds);
+            this.Controls.Add(this.lbMinutes);
+            this.Controls.Add(this.lbHours);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbHours;
+        private System.Windows.Forms.Label lbMinutes;
+        private System.Windows.Forms.Label lbSeconds;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lstBxElapsed;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbElapsed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch0003
+{
+    public partial class Form1 : Form
+    {
+        int sec, min, hrs, elapsedTime;
+        bool isRunning;
+        public Form1()
+        {
+            InitializeComponent();
+            sec = min = hrs = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            sec++;
+            if (sec >= 60)
+            {
+                min++;
+                sec = 0;
+            }
+            if (min >= 60)
+            {
+                hrs++;
+                min = 0;
+            }
+
+            lbSeconds.Text = formatter(sec);
+            lbMinutes.Text = formatter(min);
+            lbHours.Text = formatter(hrs);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Start();
+            isRunning = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            if (isRunning)
+            {
+                elapsedTime = (hrs * 3600 + min * 60 + sec);
+
+                lstBxElapsed.Items.Add(formatter(elapsedTime));
+
+                lbElapsed.Text = formatter(elapsedTime);
+
+                sec = min = hrs = 0;
+                
+                lbSeconds.Text = formatter(sec);
+                lbMinutes.Text = formatter(min);
+                lbMinutes.Text = formatter(hrs);
+            }
+
+            isRunning = false;
+        }
+        public string formatter(int num)
+        {
+            return String.Format("{0:00}", num); 
+        }
+    }
+}
+
